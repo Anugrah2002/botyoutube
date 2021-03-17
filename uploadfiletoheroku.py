ec26b99c90a9b295b8659b749ac5e1dbdf8b0b49
anugrah2002 <anugrahnigam16@gmail.com>
2021-03-18 00:04:25 +0530

    bott

--- uploadfiletoheroku.py
+++ uploadfiletoheroku.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse, JsonResponse
+# from django.http import HttpResponse, JsonResponse
 import requests
+import cloudinary.uploader
+
+
 
 
 def uploadvideotoheroku(filepath,YTtitle):
@@ -10,9 +13,14 @@ def uploadvideotoheroku(filepath,YTtitle):
         filepath="final.mp4"
         print(filepath)
         myurl = 'https://youtuberestframework.eu-gb.cf.appdomain.cloud/videouploadwithcloudinary/'
-        video = {'video': open(filepath, 'rb')}
-        title={'title':YTtitle}
-        getdata = requests.post(myurl,data=title, files=video)
+        upload_data = cloudinary.uploader.upload_large(filepath)
+        videoPublicId=str(upload_data['public_id'])
+        videoUrl=str(upload_data['secure_url'])
+        print(videoPublicId)
+        print(videoUrl)
+        title=YTtitle
+        
+        getdata = requests.post(myurl,data={'title':title,'videoPublicId':videoPublicId,'videoUrl':videoUrl})
         print(getdata.text)  
         # r=requests.post('http://lit-sierra-15246.herokuapp.com/videoupload/')
     except Exception as e:
