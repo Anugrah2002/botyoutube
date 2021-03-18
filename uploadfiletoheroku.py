d6b18a2d3df8426bb58884d21b8dddd3d84d2955
anugrah2002 <anugrahnigam16@gmail.com>
2021-03-18 16:41:46 +0530

    automatic push

--- uploadfiletoheroku.py
+++ uploadfiletoheroku.py
@@ -19,14 +19,13 @@ def uploadvideotoheroku(filepath,YTtitle):
         # filepath = filepath.replace(' ','\ ')
         filepath="final.mp4"
         print(filepath)
-        myurl = 'https://youtuberestframework.eu-gb.cf.appdomain.cloud/videouploadwithcloudinary/'
+        myurl = 'http:/ytserver.eu-gb.cf.appdomain.cloud/videouploadwithcloudinary/'
         upload_data = cloudinary.uploader.upload_large(filepath)
         videoPublicId=str(upload_data['public_id'])
         videoUrl=str(upload_data['secure_url'])
         print(videoPublicId)
         print(videoUrl)
         title=YTtitle
-        
         getdata = requests.post(myurl,data={'title':title,'videoPublicId':videoPublicId,'videoUrl':videoUrl})
         print(getdata.text)  
         # r=requests.post('http://lit-sierra-15246.herokuapp.com/videoupload/')
