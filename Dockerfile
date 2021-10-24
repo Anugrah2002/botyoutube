8b022d58be14e627d0b7b11b97385420e4ff9d80
mashuk999 <mashuk1998@gmail.com>
2021-10-24 21:13:50 +0530

    automatic push

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM wlserver1/ytupload:latest
 ADD ./ /
 RUN pip3 install cloudinary
-RUN python3 -m pip install --upgrade gtts
-RUN python3 -m pip install --upgrade gtts-token
-
 RUN python3 views.py
--- makeVideos.py
+++ makeVideos.py
@@ -7,7 +7,6 @@ import urllib.request
 from gtts import gTTS,tts
 import shutil
 from moviepy.editor import *
-from moviepy.audio.fx.multiply_volume import multiply_volume
 import settings
 
 
@@ -136,7 +135,7 @@ def addAudioToVideo(name):
     try:
         os.chdir(os.path.join(settings.BASE_DIR, r"RequiredFiles/"))
         bgAudio = AudioFileClip('bgAudioNews.mp3')
-        bgAudio = bgAudio.fx(multiply_volume, 0.5)
+        bgAudio = bgAudio.fx(afx.volumex, 0.5)
         os.chdir(os.path.join(settings.BASE_DIR, r"dataset/"+name))
         print(os.listdir())
         audiofile = AudioFileClip('audio.mp3')
