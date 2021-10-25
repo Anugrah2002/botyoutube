27c69377b0a23761cd07e874c33ef1abf98eec0f
Anugrah2002 <62788078+Anugrah2002@users.noreply.github.com>
2021-10-25 16:04:34 +0530

    Update views.py

--- views.py
+++ views.py
@@ -21,7 +21,7 @@ def checktime():
     datime=datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
     print(datime)
     dateee=datetime.datetime.strptime(datime,"%Y-%m-%d %H:%M:%S")
-    if (nextran < dateee) or True:
+    if (nextran < dateee):
        print("We will post video")
        requestVideo()
     else:
