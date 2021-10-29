0ca8da4e9b89d33eee0e24c17aa408a85fcd9a80
mashuk999 <mashuk1998@gmail.com>
2021-10-29 23:11:36 +0530

    Added bash conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,16 @@ PATH="/root/.poetry/bin:$PATH"
 git clone https://github.com/ContentAutomation/YouTubeUploader.git
 cd YouTubeUploader
 poetry install
-cd /
 ls
 apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 apt-get install docker-ce docker-ce-cli containerd.io -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
-docker-compose up -d
+docker-compose up -ds
