6e2f9b4ffc783fa612ba9849c61b961957e3ad08
mashuk999 <mashuk1998@gmail.com>
2021-10-29 23:44:35 +0530

    Added bash conf

--- views.py
+++ views.py
@@ -73,10 +73,10 @@ def requestVideo():
 
         #command = 'python ./bott/uploadToYT.py --file="'+str(p)+'" --title="'+YTtitle+'" --description="'+(summary+'\n'+credit)+'" --keywords="'+keywords+',hour news,news" --category="24" --privacyStatus="public" --noauth_local_webserver ' 
         #uploadvideotoheroku(p,YTtitle)
-        command = r'cp ' + p + r' /YouTubeUploader/uploads/'
+        command = r'cp "' + p + r'" /YouTubeUploader/uploads/'
         os.system(command)
-
-        command = r'/root/.poetry/bin/poetry run python3 /YouTubeUploader/main.py -l login.json -t ' + newYTtitle + r' -d ' + newYTtitle + ' ' + r'/uploads/final.mp4'
+        os.chdir(r'/YouTubeUploader/')
+        command = r'/root/.poetry/bin/poetry run python3 main.py -l login.json -t ' + newYTtitle + r' -d ' + newYTtitle + ' ' + r'/uploads/final.mp4'
         os.system(command) #comment this to stop uploading to youtube
         # shutil.rmtree(os.path.join(settings.BASE_DIR, r"dataset")) # comment this to stop removing the file from system
         print('Success')
